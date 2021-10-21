77e29a58763d919f3b386a0f0f4b30f7704323ab
phamflam <lynnphamthu@gmail.com>
2021-10-21 18:28:59 +0200

    initialize maven project with spring

---
+++ KBE-TaLy/src/main/java/TaLy/KBETaLy/KbeTaLyApplication.java
@@ -0,0 +1,13 @@
+package TaLy.KBETaLy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeTaLyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeTaLyApplication.class, args);
+	}
+
+}
---
+++ KBE-TaLy/src/test/java/TaLy/KBETaLy/KbeTaLyApplicationTests.java
@@ -0,0 +1,13 @@
+package TaLy.KBETaLy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeTaLyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
